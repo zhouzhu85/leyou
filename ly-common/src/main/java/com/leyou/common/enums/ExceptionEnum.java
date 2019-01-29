1818d93b4f1a4258eeee15a1eadddd03ae72e5c6
zhouzhu85 <734837014@qq.com>
2019-01-29 18:00:35 +0800

    添加工具类和异常类

---
+++ ly-common/src/main/java/com/leyou/common/enums/ExceptionEnum.java
@@ -0,0 +1,15 @@
+package com.leyou.common.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public enum  ExceptionEnum {
+
+    ;
+    private int code;
+    private String msg;
+}
