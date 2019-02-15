c32a2388f23092b5934606a4b9e317950a7ac59e
zhouzhu85 <734837014@qq.com>
2019-02-15 18:04:30 +0800

    实现图片上传

--- ly-common/src/main/java/com/leyou/common/enums/ExceptionEnum.java
+++ ly-common/src/main/java/com/leyou/common/enums/ExceptionEnum.java
@@ -10,7 +10,9 @@ import lombok.NoArgsConstructor;
 public enum  ExceptionEnum {
     CATEGORY_NOT_FOND(404,"商品分类没查到"),
     BRAND_NOT_FOUND(404,"商品分类没查到"),
-    BRAND_SAVE_ERROR(500,"新增品牌失败")
+    BRAND_SAVE_ERROR(500,"新增品牌失败"),
+    UPLOAD_FILE_ERROR(500,"文件上传失败"),
+    INVALID_FILE_TYPE(400,"无效的文件类型")
     ;
     private int code;
     private String msg;
