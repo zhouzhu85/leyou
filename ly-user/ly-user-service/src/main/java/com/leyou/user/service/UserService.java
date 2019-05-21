4d0ef7c1c2f770425cb22691a5deedae462f8a7d
zhouzhu85 <734837014@qq.com>
2019-05-21 18:01:23 +0800

    用户模块--短信验证码接口

--- ly-user/ly-user-service/src/main/java/com/leyou/user/service/UserService.java
+++ ly-user/ly-user-service/src/main/java/com/leyou/user/service/UserService.java
@@ -2,10 +2,19 @@ package com.leyou.user.service;
 
 import com.leyou.common.enums.ExceptionEnum;
 import com.leyou.common.exception.LyException;
+import com.leyou.common.utils.NumberUtils;
 import com.leyou.user.mapper.UserMapper;
 import com.leyou.user.pojo.User;
+import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.TimeoutUtils;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author zhouzhu
@@ -16,6 +25,14 @@ import org.springframework.stereotype.Service;
 public class UserService {
     @Autowired
     private UserMapper userMapper;
+
+    @Autowired
+    private AmqpTemplate amqpTemplate;
+
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+
+    private static final String KEY_PREFIX="user:verify:phone";
 
     public Boolean checkData(String data, Integer type) {
         User record=new User();
@@ -31,5 +48,20 @@ public class UserService {
                 throw new LyException(ExceptionEnum.INVALID_USER_DATA_TYPE);
         }
         return userMapper.selectCount(record)==0;
+    }
+
+    public void sendCode(String phone) {
+        //生成key
+        String key=KEY_PREFIX+phone;
+        //生成验证码
+        String code= NumberUtils.generateCode(6);
+
+        Map<String,String> msg=new HashMap<>();
+        msg.put("phone",phone);
+        msg.put("code",code);
+        //发送验证码
+        amqpTemplate.convertAndSend("ly.sms.exchange","sms.verify.code",msg);
+        //保存验证码
+        redisTemplate.opsForValue().set(key,code,5, TimeUnit.MINUTES);
     }
 }
--- ly-user/ly-user-service/src/main/java/com/leyou/user/web/UserController.java
+++ ly-user/ly-user-service/src/main/java/com/leyou/user/web/UserController.java
@@ -2,10 +2,9 @@ package com.leyou.user.web;
 
 import com.leyou.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author zhouzhu
@@ -26,5 +25,16 @@ public class UserController {
     @GetMapping("/check/{data}/{type}")
     public ResponseEntity<Boolean> checkData(@PathVariable("data") String data,@PathVariable("type") Integer type){
         return ResponseEntity.ok(userService.checkData(data,type));
+    }
+
+    /**
+     * 发送短信验证码
+     * @param phone
+     * @return
+     */
+    @PostMapping("code")
+    public ResponseEntity<Void> sendCode(@RequestParam("phone")String phone){
+        userService.sendCode(phone);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
