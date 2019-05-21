f68a7e0f109258359b5709d552591a19a5dac2a7
zhouzhu85 <734837014@qq.com>
2019-05-21 16:25:51 +0800

    用户模块--服务模块

---
+++ ly-user/ly-user-service/src/main/java/com/leyou/LyUserApplication.java
@@ -0,0 +1,20 @@
+package com.leyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import tk.mybatis.spring.annotation.MapperScan;
+
+/**
+ * @author zhouzhu
+ * @Description
+ * @create 2019-05-21 16:14
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@MapperScan("com.leyou.user.mapper")
+public class LyUserApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LyUserApplication.class,args);
+    }
+}
